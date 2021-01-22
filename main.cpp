a7ddb3fef89b1fbf85867a818c321d15c6bfc449
VictorinaVicka <city_br@mail.ru>
2021-01-22 15:33:12 +0300

    test_list

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfarenga <tfarenga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/22 15:19:08 by tfarenga          #+#    #+#             */
+/*   Updated: 2021/01/22 15:25:01 by tfarenga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Test.hpp"
+
+int main()
+{
+	// std::cout << ":: Vector ::" << std::endl;
+	// test_vector();
+	std::cout << ":: List ::" << std::endl;
+	test_list();
+	// std::cout << ":: Stack ::" << std::endl;
+	// test_stack();
+	// std::cout << ":: Queue ::" << std::endl;
+	// test_queue();
+	// std::cout << ":: Map ::" << std::endl;
+	// test_map();
+	// std::cout << ":: Set ::" << std::endl;
+	// test_set();
+	// std::cout << ":: MultiMap ::" << std::endl;
+	// test_multimap();
+	// std::cout << ":: MultiSet ::" << std::endl;
+	// test_multiset();
+	// std::cout << ":: Deque ::" << std::endl;
+	// test_deque();
+	// std::cout << ":: Everything is ok! ::" << std::endl;
+	return 0;
+}
